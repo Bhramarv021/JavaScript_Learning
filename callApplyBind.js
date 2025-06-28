b05086993e4a6850bea6b1b5f8e21dc70ed31a57
Bhramarv021 <Bhramarv021@gmail.com>
2025-06-28 16:16:20 +0530

    Details added for call, bind and apply

--- callApplyBind.js
+++ callApplyBind.js
@@ -12,8 +12,16 @@ const student2 = {
     lName: "virmani"
 }
 
+//call - Invokes a function immediately with a specified this context and arguments passed individually.
+//Syntax: function.call(thisArg, arg1, arg2, ...)
 printStudentDetails.call(student, "delhi", "pune");
 printStudentDetails.call(student2);
+
+//apply -  Similar to call, but arguments are passed as an array instead of individually.
+//Syntax: function.apply(thisArg, [arg1, arg2, ...])
 printStudentDetails.apply(student2, ["delhi", "pune"]);
+
+//bind - Does not invoke the function immediately. Instead, it returns a new function with a specified this context and optionally pre-filled arguments.
+//Syntax: const boundFunction = function.bind(thisArg, arg1, arg2, ...)
 const callStudentDetails = printStudentDetails.bind(student2, "delhi", "pune");
 callStudentDetails();
